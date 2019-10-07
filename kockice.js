8e651acd2b075f56578495002b2c7b60a91515c7
Tadija Selakovic <selakovic.tadija@gmail.com>
2019-10-07 18:20:54 +0200

    Promena boje na klik

--- kockice.js
+++ kockice.js
@@ -1,18 +1,29 @@
 var canvas = new fabric.Canvas("theBigBox");
-
-for (var i = 0; i < 1000; i += 100) {
-  for (var j = 0; j < 1000; j += 100) {
+var indexOfRect=0;
+for (var i = 0; i < 10; i += 1) {
+  for (var j = 0; j < 10; j += 1) {
     addNewRect(i,j);
   }
 }
 function addNewRect(i,j){
+  indexOfRect++;
     var rect = new fabric.Rect({
-      left: j,
-      top: i,
+id:indexOfRect,
       fill: "#fbfcf2",
       width: 100,
       height: 100,
       stroke: "#eee"
     });
-    canvas.add(rect);
+    
+    var text40 = new fabric.Text(indexOfRect.toString(), {
+      fontSize: 40
+    });
+    var group = new fabric.Group([ rect, text40 ], {
+      left: j*100,
+      top: i*100,
+    });
+    group.on('selected', function() {
+     rect.set("fill", "#276b68");
+    });
+    canvas.add(group);
 }
