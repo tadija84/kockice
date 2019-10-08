6b036c525649a4523ec25576922e3a8131169063
Tadija Selakovic <selakovic.tadija@gmail.com>
2019-10-08 03:10:21 +0200

    podesena boja dozvoljenih polja

--- kockice.js
+++ kockice.js
@@ -15,48 +15,70 @@ function addNewRect(i, j) {
     height: 100,
     stroke: "#eee",
     left: j * 100,
-    top: i * 100
+    top: i * 100,
+    i: i + 1,
+    j: j + 1
   });
 
-  
- 
   rect.on("selected", function() {
     if (clickCounter == 0) {
       clickingCons(rect);
     } else {
-      if (rect.fill == "#d0d765") {     
+      if (rect.fill == "#d0d765") {
         clickingCons(rect);
       } else {
-        return
+        return;
       }
     }
   });
   canvas.add(rect);
 }
 
-function clickingCons(rect) {  
-  console.log("ahahaha")
-  staGod();
+function clickingCons(rect) {
+  removeMarks();
+  markingClickable(rect.i, rect.j);
   rect.set("fill", "#276b68");
   clickCounter++;
-  
-
+  if(clickCounter==100){
+    alert("Cestitamo. Pobedili ste.");
+  }
 }
-function staGod(){
-  var probni=canvas.getObjects();
- 
- probni[1].set("fill", "#d0d765")
- console.log(probni[1]);
-}
-
-
-
-function odabirPoID(x,y){
-  var allBoxes=canvas.getObjects();
-  var id=x*10+y;
-  for(i=0;i<allBoxes.length;i++){
-    if(allBoxes[i].id=id){
-      return allBoxes[i]
+function removeMarks(){
+  var allBoxes = canvas.getObjects();
+  for (i = 0; i < allBoxes.length; i++) {
+    if (allBoxes[i].fill=="#d0d765") {
+      allBoxes[i].set("fill", "#fbfcf2");
     }
   }
+}
+function markingClickable(valY, valX) {
+  makeItYellow(valY-3, valX);
+  makeItYellow(valY-2,valX+2);
+  makeItYellow(valY,valX+3);
+  makeItYellow(valY+2,valX+2);
+  makeItYellow(valY+3,valX);
+  makeItYellow(valY+2,valX-2);
+  makeItYellow(valY,valX-3);
+  makeItYellow(valY-2,valX-2);
+}
+function makeItYellow(x, y) {
+  var wantedRect = selectById(x, y);
+  if (wantedRect !== undefined) {
+    if(wantedRect.fill !== "#276b68"){
+ wantedRect.set("fill", "#d0d765");
+    }  
+  }
+}
+
+function selectById(x, y) {
+  var allBoxes = canvas.getObjects();
+  if(y>0){
+    var id = (x - 1) * 10 + y;
+  for (i = 0; i < allBoxes.length; i++) {
+    if (id==allBoxes[i].id) {
+      return allBoxes[i];
+    }
+  }
+  }
+  
 }
